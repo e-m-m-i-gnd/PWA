177fa9c6f24ef9671df88bafec601c2b12ded2d9
Diego Emiliano GN <diegogemiliano@gmail.com>
2024-10-17 19:30:07 -0600

    servicer-worker.js
    Segunda prueba

--- servicer-worker.js
+++ servicer-worker.js
@@ -1,6 +1,4 @@
 const CACHE_NAME = 'pwa-cache-v1';
-
-// Lista de archivos que queremos almacenar en la caché
 const FILES_TO_CACHE = [
   '/',
   '/index.html',
@@ -9,25 +7,23 @@ const FILES_TO_CACHE = [
   '/images/icon-512x512.png'
 ];
 
-// Evento de instalación
+// Instalación del Service Worker
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => {
-        console.log('Archivos enviados a caché correctamente');
         return cache.addAll(FILES_TO_CACHE);
       })
   );
 });
 
-// Evento de activación
+// Activación del Service Worker
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames.map((cacheName) => {
           if (cacheName !== CACHE_NAME) {
-            console.log('Caché antigua eliminada');
             return caches.delete(cacheName);
           }
         })
@@ -36,12 +32,11 @@ self.addEventListener('activate', (event) => {
   );
 });
 
-// Evento fetch
+// Interceptar las solicitudes y servir los archivos en caché
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
